2152d8f6e881d1ecb16b10b23bb7b3976e3aad24
denistorjai <denistorjai@outlook.com>
2024-12-03 13:48:57 -0700

    Update route.ts

---
+++ app/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+if (!process.env.ClientId || !process.env.ClientSecret) {
+    throw new Error("error");
+}
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.ClientId,
+      clientSecret: process.env.ClientSecret
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
