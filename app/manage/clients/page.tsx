b26056c16397c163222116073fb60d6d516f0a5f
denistorjai <denistorjai@outlook.com>
2024-12-04 16:39:47 -0700

    Navbar Update + PageName Icons

--- app/manage/clients/page.tsx
+++ app/manage/clients/page.tsx
@@ -3,28 +3,13 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import NavMenu from "@/app/components/navmenu";
+import PageName from "@/app/components/pagename";
 
-export default function Home() {
-  
-  // Session
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  // Push to login if unauthenticated
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      router.push("/login");
-    }
-  }, [status, router]);
-
-  if (status === "loading") {
-    return <div className="h-screen w-screen flex justify-center items-center" >Loading...</div>;
-  }
+export default function Clients() {
 
   return (
     <div>
-        <NavMenu/>
+      <PageName PageName="Clients" />
     </div>
   );
 
