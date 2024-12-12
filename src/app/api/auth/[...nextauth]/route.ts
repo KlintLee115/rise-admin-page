d2bca0b13b73b439b5248f8a6f7fd759ee3a7920
KlintLee115 <klintlee1@gmail.com>
2024-12-11 18:03:10 -0700

    renamed google secrets

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,18 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-if (!process.env.GOOGLE_AUTH_ClientId || !process.env.GOOGLE_AUTH_ClientSecret) {
+if (!process.env.GOOGLE_AUTH_CLIENTID || !process.env.GOOGLE_AUTH_CLIENTSECRET) {
     throw new Error("error");
 }
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_AUTH_ClientId,
-      clientSecret: process.env.GOOGLE_AUTH_ClientSecret
+      clientId: process.env.GOOGLE_AUTH_CLIENTID,
+      clientSecret: process.env.GOOGLE_AUTH_CLIENTSECRET
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_AUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
